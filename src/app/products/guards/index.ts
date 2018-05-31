5b408242d0a79ce314bec01ca54fb13e5c5f1ad8
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-05-31 19:25:45 +0300

    Toppings Guard added

--- src/app/products/guards/index.ts
+++ src/app/products/guards/index.ts
@@ -1,7 +1,9 @@
 import { PizzasGuard } from './pizzas.guard';
 import { PizzaExistsGuards } from './pizza-exists.guard';
+import { ToppingsGuard } from './toppings.guard';
 
-export const guards = [PizzasGuard, PizzaExistsGuards];
+export const guards = [PizzasGuard, PizzaExistsGuards, ToppingsGuard];
 
 export * from './pizzas.guard';
 export * from './pizza-exists.guard';
+export * from './toppings.guard';
