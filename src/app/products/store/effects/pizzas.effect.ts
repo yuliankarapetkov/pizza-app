b571495be28bfa12fe518f813d1a72a530562a6a
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2018-05-31 15:55:25 +0300

    Added routing effects for successful create, update and remove of a pizza

--- src/app/products/store/effects/pizzas.effect.ts
+++ src/app/products/store/effects/pizzas.effect.ts
@@ -4,6 +4,7 @@ import { Effect, Actions } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, switchMap, catchError } from 'rxjs/operators';
 
+import * as fromRoot from '../../../store';
 import * as pizzaActions from '../actions/pizzas.action';
 import * as fromServices from '../../services';
 
@@ -41,6 +42,19 @@ export class PizzasEffects {
         );
 
     @Effect()
+    createPizzaSuccess$ = this.actions$
+        .ofType(pizzaActions.CREATE_PIZZA_SUCCESS)
+        .pipe(
+            map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+            map(pizza => {
+                return new fromRoot.Go({
+                    path: ['/products', pizza.id]
+                });
+            })
+        );
+
+
+    @Effect()
     updatePizza$ = this.actions$
         .ofType(pizzaActions.UPDATE_PIZZA)
         .pipe(
@@ -65,6 +79,19 @@ export class PizzasEffects {
                         map(() => new pizzaActions.RemovePizzaSuccess(pizza)),
                         catchError(error => of(new pizzaActions.RemovePizzaFail(error)))
                     );
+            })
+        );
+
+    @Effect()
+    handlePizzaSuccess$ = this.actions$
+        .ofType(
+            pizzaActions.UPDATE_PIZZA_SUCCESS,
+            pizzaActions.REMOVE_PIZZA_SUCCESS)
+        .pipe(
+            map(() => {
+                return new fromRoot.Go({
+                    path: ['/products']
+                });
             })
         );
 }
